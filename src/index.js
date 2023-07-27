e85581d0fa1b770e2cfb53aaa3521643d8c7f5fa
hamitmizrak <hamitmizrak@gmail.com>
2023-07-27 20:14:10 +0300

    component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RouterBlog from './RouterBlog';
 
+// BrowserRouter
+// BrowserRouter  ==> http://localhost:3000/
+// HashRouter  ==> http://localhost:3000/#/
+import { BrowserRouter } from 'react-router-dom';
+
+// ROOT
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// RENDER
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RouterBlog />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
