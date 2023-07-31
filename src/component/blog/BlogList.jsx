ee291c597bfcb204cf1bc564cff16c2c3d49ac39
hamitmizrak <hamitmizrak@gmail.com>
2023-07-31 21:19:05 +0300

    BlogList

--- src/component/blog/BlogList.jsx
+++ src/component/blog/BlogList.jsx
@@ -1,9 +1,56 @@
 import React, { Component } from 'react'
 
-export default class BlogList extends Component {
-  render() {
-    return (
-      <div>BlogList</div>
-    )
+// i18n
+import { withTranslation } from 'react-i18next'
+
+// Blog Api
+import BlogApi from '../../services/BlogApi';
+
+// CLASS 
+class BlogList extends Component {
+
+  // Componentteki isim
+  static displayName = "Blog_List";
+
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    // STATE
+    this.state = {
+      blogList: [],
+    }; //end constructor
+
+    // BIND
   }
-}
+
+  // CDM
+  componentDidMount() {
+    //const url='https://jsonplaceholder.typicode.com/posts';
+    BlogApi.blogServiceList()
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          blogList: response.data
+        }); //end setState
+      })
+      .catch((err) => {
+        console.log('Error:', err)
+      }
+      );
+  } //end CDM
+
+  // FUNCTION
+
+  //RENDER
+  render() {
+
+    //RETURN 
+    return (
+      <React.Fragment>BlogList</React.Fragment>
+    ) //end return
+  } //end render
+} // end class
+
+// 
+export default withTranslation()(BlogList);
--- src/internationalization/OtherLanguageReusability.jsx
+++ src/internationalization/OtherLanguageReusability.jsx
@@ -26,7 +26,7 @@ function OtherLanguageReusability(props) {
     // RETURN
     return (
         <React.Fragment>
-            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
                 <li className="nav-item">
                     <Link className="nav-link" onClick={() => internationalizationLanguageServise('tr')}>
                         <img src={tr_flag} style={{height:"20px"}} alt='tr' />
