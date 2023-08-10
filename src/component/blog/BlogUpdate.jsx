65459cba9dc7869ba27b7ca3f285a36c6a364410
Hamit Mızrak <hamitmizrak@gmail.com>
2023-08-10 20:30:06 +0300

    BlogView

--- src/component/blog/BlogUpdate.jsx
+++ src/component/blog/BlogUpdate.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from 'react'
+import { withTranslation } from 'react-i18next';
 
-export default class BlogUpdate extends Component {
-  render() {
-    return (
-      <div>BlogUpdate</div>
-    )
+// CLASS
+class BlogUpdate extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    // STATE
+    this.state = {
+
+    }; //end constructor
+
+    // BIND
   }
-}
+
+  // CDM
+
+  // FUNCTION
+
+  // Componentteki isim
+  static displayName = "Blog_Update";
+
+  // RENDER
+  render() {
+
+    // RETURN
+    return (
+      <React.Fragment>
+        view
+      </React.Fragment>
+    ) //end return
+  } //end render
+} // end class
+
+// Blog Create
+export default withTranslation()(BlogUpdate);
--- src/component/blog/BlogView.jsx
+++ src/component/blog/BlogView.jsx
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
+import { withTranslation } from 'react-i18next';
 
-export default class BlogView extends Component {
-  render() {
-    return (
-      <div>BlogView</div>
-    )
+// CLASS
+class BlogView extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    // STATE
+    this.state = {
+      //id:this.props.value.matches[0].params.id,
+
+    }; //end constructor
+
+    // BIND
   }
-}
+
+  // CDM
+
+  // FUNCTION
+
+  // Componentteki isim
+  static displayName = "Blog_View";
+
+  // RENDER
+  render() {
+
+    // RETURN
+    return (
+      <React.Fragment>
+        view
+      </React.Fragment>
+    ) //end return
+  } //end render
+} // end class
+
+// Blog Create
+export default withTranslation()(BlogView);
